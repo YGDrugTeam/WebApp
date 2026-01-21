42ff4bb67f2a0d47f0565b67a005900f705855a3
Taek-Su Kim <taeksu880@gmail.com>
2026-01-21 13:51:57 +0900

    260121 13:51 ai 의약품 서비스 프론트엔드, 백엔드 개발

--- pill-safe-ai/backend/main.py
+++ pill-safe-ai/backend/main.py
@@ -1,3 +1,6 @@
+import model
+import numpy as np
+import cv2
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -6,10 +9,17 @@ app = FastAPI()
 @app.post("/analyze")
 async def analyze_pill(file: UploadFile = File(...)):    
     contents = await file.read()
+
+    # analyze_pill 함수 안에서 추가될 내용
+    image_bytes = np.frombuffer(contents, np.uint8) 
+    img = cv2.imdecode(image_bytes, cv2.IMREAD_COLOR)
+    
     return {
         "filename": file.filename,
         "size": len(contents), # 파일 크기(byte)
-        "message": "분석 준비 완료"
+        "message": "AI가 이미지를 확인했습니다!",
+        "detected_text": "알약 이름을 읽는 중...", # 여기에 결과가 담길 예정입니다.
+        "pill_name": ai_result
     }
 
 app.add_middleware(
@@ -18,4 +28,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-    
+
+result = model.ocr_reader(img)
